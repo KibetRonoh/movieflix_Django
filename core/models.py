104c4629bda245ef57964a4d7b6585a0bdb102d4
‘Kibet <kibethillaire@gmail.com>
2021-11-21 19:45:29 +0300

    update Model

--- core/models.py
+++ core/models.py
@@ -37,6 +37,17 @@ class Movie(models.Model):
     age_limit = models.CharField(max_length=5, choices=AGE_CHOICES, blank=True, null=True)
 
 
+class Series(models.Model):
+    title: str = models.CharField(max_length=225)
+    description: str = models.TextField(max_length=1000)
+    created = models.DateTimeField(auto_now_add=True)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+    type = models.CharField(max_length=10, choices=MOVIE_TYPE)
+    videos = models.ManyToManyField('Video')
+    flyer = models.ImageField(upload_to='flyers', blank=True, null=True)
+    age_limit = models.CharField(max_length=5, choices=AGE_CHOICES, blank=True, null=True)
+
+
 class Video(models.Model):
     title: str = models.CharField(max_length=225, blank=True, null=True)
     file = models.FileField(upload_to='movies')
