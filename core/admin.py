528f8d4bc0b2d9dd14d21c0e6fe0172843c19fd3
kibetronoh <kibethillaire@gmail.com>
2021-12-07 20:20:38 +0300

    update

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,3 @@ from .models import CustomUser,Profile,Movie,Video
 
 # Register your models here.
 admin.site.register(CustomUser)
-admin.site.register(Profile)
-admin.site.register(Movie)
-admin.site.register(Video)
